e5907ef1706594ede68a28fdeadb766b267b8e1a
shunl2 <88065966+shunl2@users.noreply.github.com>
2021-07-27 18:43:42 -0700

    Update views.py

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context={'text':"hello world", 'number':100}
+    context={'text':"hello world2", 'number':100}
     return render(request,'basic_app/index.html', context)
 
 def other(request):
